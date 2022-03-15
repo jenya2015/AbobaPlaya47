34ca187ca9967369f9d9fe37337953d85d6ed202
jenya2015 <banaev1997@bk.ru>
2022-03-15 22:14:12 +0300

    hgjhgjghj

---
+++ AbobaPlaya47/app/src/main/java/com/example/abobaplaya47/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.abobaplaya47;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.net.Uri;
+import android.view.View;
+import android.widget.VideoView;
+import android.widget.MediaController;
+
+public class MainActivity extends AppCompatActivity {
+
+    VideoView videoPlayer;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        videoPlayer = findViewById(R.id.videoPlayer);
+        Uri myVideoUri= Uri.parse( "android.resource://" + getPackageName() + "/" + R.raw.mem);
+        videoPlayer.setVideoURI(myVideoUri);
+        MediaController mediaController = new MediaController(this);
+        videoPlayer.setMediaController(mediaController);
+        mediaController.setMediaPlayer(videoPlayer);
+    }
+
+    public void play(View view){
+        videoPlayer.start();
+    }
+    public void pause(View view){
+        videoPlayer.pause();
+    }
+    public void stop(View view){
+        videoPlayer.stopPlayback();
+        videoPlayer.resume();
+    }
+}
